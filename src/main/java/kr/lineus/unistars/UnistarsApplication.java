9cab0b294c4b486118f1f7d8b5c259a87fba2820
lineuslocal <60596220+lineuslocal@users.noreply.github.com>
2020-02-03 17:48:54 +0700

    1st commit springboot boilerplate code for back-end part

---
+++ src/main/java/kr/lineus/unistars/UnistarsApplication.java
@@ -0,0 +1,13 @@
+package kr.lineus.unistars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnistarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnistarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/lineus/unistars/UnistarsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.lineus.unistars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnistarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
